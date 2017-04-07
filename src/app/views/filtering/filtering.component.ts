d42d32a96ccea3f5b974255949520be978ceba31
JAGFx <contact@jagfx.fr>
2017-04-07 13:06:32 +0200

    Feature Filtering: Add form for research entity with property

--- src/app/views/filtering/filtering.component.ts
+++ src/app/views/filtering/filtering.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component( {
 	selector:    'app-filtering',
 	templateUrl: 'filtering.component.html'
 } )
-export class FilteringComponent implements OnInit {
+export class FilteringComponent {
+	public filterForm = {};
 	
-	constructor() {
-	}
-	
-	ngOnInit() {
+	public validate() {
+		console.log( this.filterForm );
 	}
 	
 }
