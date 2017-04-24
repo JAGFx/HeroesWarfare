ed9e4cf1e56c0872e423778b3e4556c0ce2110a1
JAGFx <contact@jagfx.fr>
2017-04-24 07:57:33 +0200

    Doc: Addd doc for BaseEntity & BaseForm

--- src/app/components/commons/base-entity.ts
+++ src/app/components/commons/base-entity.ts
@@ -2,21 +2,48 @@
  * Created by SMITHE on 13-Mar-17.
  */
 
+/**
+ * BaseEntity are an abstract class for all entity of project. It's created to provide TRUE object due to JavaScript limitation
+ */
 export abstract class BaseEntity {
 	
 	protected _id: string;
 	
+	/**
+	 * Copy an entity from another
+	 *
+	 * @param {any} entity Entity to copy
+	 */
 	public abstract copyFrom( entity: any );
 	
-	public abstract serialize(): any;
+	/**
+	 * Serialize current object in JSON string
+	 *
+	 * @return {string} Object serialized
+	 *
+	 */
+	public abstract serialize(): string;
 	
+	/**
+	 * Compare if two object are equal
+	 *
+	 * @param {any} entity Object to compare
+	 * @return {boolean} True if two
+	 */
 	public abstract equal( entity: any ): boolean;
 	
+	/**
+	 * Generate unique ID for entity. Will be remove with API interaction
+	 *
+	 * @returns {number} UUID
+	 */
 	public static generateUUID(): any {
 		return Math.round( Math.random().valueOf() * ( 10 ** 16 ) )
 			+ Math.round( Math.random().valueOf() * ( 10 ** 16 ) );
 	}
 	
+	
+	// ----------------------------------------------------------------------- GETTERS & SETTERS
 	public get id(): string {
 		return this._id;
 	}
--- src/app/components/commons/base-form.ts
+++ src/app/components/commons/base-form.ts
@@ -1,35 +1,72 @@
-import { FormBuilder, FormGroup } from '@angular/forms';
 import { EventEmitter, Output } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 /**
  * Created by SMITHE on 13-Mar-17.
  */
 
 export abstract class BaseFormComponent<T> {
+	/**
+	 * Class entity
+	 * @type {T}
+	 */
 	public entity: T;
-	public feedback   = {
+	
+	/**
+	 *
+	 */
+	public entityBack: T;
+	public feedback = {
 		asError: false,
 		type:    'error',
 		message: ''
 	};
 	
+	protected form: FormGroup;
+	
+	/**
+	 * Object to pass data through an event
+	 *
+	 * @type {EventEmitter}
+	 */
 	@Output()
 	public onValidate: EventEmitter<T> = new EventEmitter();
 	
-	public entityBack: T;
-	protected form: FormGroup;
-	
+	/**
+	 *  Init current class entity
+	 *
+	 * @param {T} entity Entity to set in class entity
+	 */
 	public abstract init( entity: T ): void;
 	
+	/**
+	 *  Update form entity
+	 */
 	protected abstract updateForm(): void;
 	
+	/**
+	 *  Call when form entity changes. Use to catch errors thrown
+	 *
+	 * @param value Current value in form
+	 */
 	protected abstract onChangeEntity( value ): void;
 	
+	/**
+	 * Build form to use it after
+	 *
+	 * @param fb  Object FormBuilder with fields necessary
+	 */
 	protected abstract buildForm( fb: FormBuilder ): void;
 	
+	/**
+	 * Reset current class entity with old entity
+	 */
 	public reset(): void {
 		this.init( this.entityBack );
 	}
 	
+	/**
+	 *  Call in submit form. Emit event with form entity
+	 */
 	public validate(): void {
 		this.onValidate.emit( this.entity );
 	}
